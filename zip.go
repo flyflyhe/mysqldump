ca88070db777f930454505eab71fcc62feb8e616
mrHe <hejinxue001@lonlife.cn>
2020-10-12 10:50:55 +0800

    优化unzip

--- zip.go
+++ zip.go
@@ -107,31 +107,36 @@ func UnZip(dst, src string) (err error) {
 			continue
 		}
 
-		// 获取到 Reader
-		fr, err := file.Open()
+		f := func(path string, file *zip.File) error {
+			// 获取到 Reader
+			fr, err := file.Open()
+			defer fr.Close()
+			if err != nil {
+				return err
+			}
+
+			// 创建要写出的文件对应的 Write
+			fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
+			defer fw.Close()
+
+			if err != nil {
+				return err
+			}
+
+			n, err := io.Copy(fw, fr)
+			if err != nil {
+				return err
+			}
+
+			// 将解压的结果输出
+			fmt.Printf("成功解压 %s ，共写入了 %d 个字符的数据\n", path, n)
+			return nil
+		}
+
+		err := f(path, file)
 		if err != nil {
 			return err
 		}
-
-		// 创建要写出的文件对应的 Write
-		fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
-		if err != nil {
-			return err
-		}
-
-		n, err := io.Copy(fw, fr)
-		if err != nil {
-			return err
-		}
-
-		// 将解压的结果输出
-		fmt.Printf("成功解压 %s ，共写入了 %d 个字符的数据\n", path, n)
-
-		// 因为是在循环中，无法使用 defer ，直接放在最后
-		// 不过这样也有问题，当出现 err 的时候就不会执行这个了，
-		// 可以把它单独放在一个函数中，这里是个实验，就这样了
-		fw.Close()
-		fr.Close()
 	}
 	return nil
 }
